bd29b0b46589db3e7cfc856787d9f574bad954f1
travisvyas <travisvyas@gmail.com>
2015-10-27 19:28:02 -0500

    try 5

--- lab5/src/main/java/ca/uwo/csd/cs2212/travisvyas/TestBankAccount.java
+++ lab5/src/main/java/ca/uwo/csd/cs2212/travisvyas/TestBankAccount.java
@@ -13,4 +13,13 @@ public class TestBankAccount {
     double amount = account.debit(5);
     Assert.assertEquals(15.0, amount);
   }
+
+	@Test
+	public void testDebitWithInsufficientFunds(){
+		BankAccount account = new BankAccount(10);
+		double amount = account.debit(11);
+		Assert.assertEquals(10.0, amount);
+	}
+
+
 }
