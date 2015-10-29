639589be962dc37e9ec584c4123fca9c1bd7c744
travisvyas <travisvyas@gmail.com>
2015-10-28 22:16:19 -0500

    jacoco line coverage

--- lab5/src/test/java/ca/uwo/csd/cs2212/travisvyas/TestBankAccount.java
+++ lab5/src/test/java/ca/uwo/csd/cs2212/travisvyas/TestBankAccount.java
@@ -7,12 +7,12 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
-  @Test
-  public void testDebitWithSufficientFunds(){
-    BankAccount account = new BankAccount(20);
-    double amount = account.debit(6);
-    Assert.assertEquals(6.0, amount);
-  }
+  	@Test
+  	public void testDebitWithSufficientFunds(){
+    		BankAccount account = new BankAccount(20);
+    	double amount = account.debit(6);
+    	Assert.assertEquals(6.0, amount);
+  	}
 
 	@Test
 	public void testDebitWithInsufficientFunds(){
@@ -20,6 +20,4 @@ public class TestBankAccount {
 		double amount = account.debit(11);
 		Assert.assertEquals(10.0, amount);
 	}
-
-
 }
