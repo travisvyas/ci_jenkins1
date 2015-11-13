22887877d30325da630cfc827b4e41692fcbefdf
travisvyas <tangentcore@gmail.com>
2015-11-13 13:35:38 -0600

    nov13

--- lab5/src/test/java/ca/uwo/csd/cs2212/travisvyas/TestBankAccount.java
+++ lab5/src/test/java/ca/uwo/csd/cs2212/travisvyas/TestBankAccount.java
@@ -7,17 +7,17 @@ import org.junit.Test;
 
 public class TestBankAccount {
 
-  	@Test
-  	public void testDebitWithSufficientFunds(){
-    		BankAccount account = new BankAccount(20);
-    	double amount = account.debit(6);
-    	Assert.assertEquals(6.0, amount);
-  	}
+ // 	@Test
+ // 	public void testDebitWithSufficientFunds(){
+ //   		BankAccount account = new BankAccount(20);
+ //   	double amount = account.debit(6);
+ //   	Assert.assertEquals(6.0, amount);
+ // 	}
 
-	@Test
-	public void testDebitWithInsufficientFunds(){
-		BankAccount account = new BankAccount(10);
-		double amount = account.debit(11);
-		Assert.assertEquals(10.0, amount);
-	}
+	// @Test
+	// public void testDebitWithInsufficientFunds(){
+	// 	BankAccount account = new BankAccount(10);
+	// 	double amount = account.debit(11);
+	// 	Assert.assertEquals(10.0, amount);
+	// }
 }
